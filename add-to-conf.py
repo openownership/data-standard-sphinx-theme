98b31180435bbe8e4deb6e16ccd2b51aae4f3c3e
James Baster <james.baster@opendataservices.coop>
2018-05-09 16:56:57 +0100

    These changes are now in the data-standard repo so we don't need to tell people to make the changes here

--- add-to-conf.py
+++ add-to-conf.py
@@ -9,22 +9,3 @@ html_theme = 'sphinxtheme'
 html_theme_path = [os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'oods')]
 
 
-
-###################################################### Pygments Style
-#
-# There is an existing pygments_style bit - search for it and replace it with this.
-#
-
-import oods.pygments
-oods.pygments.pygments_monkeypatch_style("oods", oods.pygments.OODSStyle)
-pygments_style = "oods"
-
-
-###################################################### Extensions
-# Remove sphinxcontrib.opendataservices - don't think it's actually used.
-#
-# There is an existing extensions bit - search for it and replace it with this.
-#
-
-extensions = ['sphinxcontrib.jsonschema']
-
