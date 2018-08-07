fbadaafcac54a0376a43131786d7419d5d0aa19d
James Baster <james.baster@opendataservices.coop>
2018-08-07 12:47:01 +0100

    Check for broken links on each build, as it's pretty quick. http://bods.opendataservices.coop/redmine/issues/168

--- vagrant/build-all.sh
+++ vagrant/build-all.sh
@@ -12,3 +12,5 @@ export PYTHONPATH=$PYTHONPATH:/vagrant/
 
 sphinx-build  -a /vagrant/data-standard/docs   /out/
 
+pylinkvalidate.py --output=/tmp/report.txt   http://localhost/ || true
+cat /tmp/report.txt
--- vagrant/build-sphinx.sh
+++ vagrant/build-sphinx.sh
@@ -6,3 +6,5 @@ export PYTHONPATH=$PYTHONPATH:/vagrant/
 
 sphinx-build  -a /vagrant/data-standard/docs   /out/
 
+pylinkvalidate.py --output=/tmp/report.txt   http://localhost/ || true
+cat /tmp/report.txt
