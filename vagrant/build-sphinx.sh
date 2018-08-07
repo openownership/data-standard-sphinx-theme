39378e43134b7c37fa5c98c89fd839ef3717993a
James Baster <james.baster@opendataservices.coop>
2018-08-07 12:50:43 +0100

    Check for broken links on each build - make sure to clear old data http://bods.opendataservices.coop/redmine/issues/168 Fix to fbadaafcac54a0376a43131786d7419d5d0aa19d

--- vagrant/build-sphinx.sh
+++ vagrant/build-sphinx.sh
@@ -6,5 +6,8 @@ export PYTHONPATH=$PYTHONPATH:/vagrant/
 
 sphinx-build  -a /vagrant/data-standard/docs   /out/
 
+
+# remove so we are certain the data we echo to the user is from this run, and not a previous run
+rm /tmp/report.txt
 pylinkvalidate.py --output=/tmp/report.txt   http://localhost/ || true
 cat /tmp/report.txt
