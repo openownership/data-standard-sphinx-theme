999b25357479a94697d4c85d28d46e27a624e7c2
James Baster <james.baster@opendataservices.coop>
2019-09-04 12:24:56 +0100

    Make add-to-conf.py instructions clearer.
    (After debugging a problem that turned out to be due to this.)

--- add-to-conf.py
+++ add-to-conf.py
@@ -4,6 +4,7 @@
 #  There is an existing html_theme bit - search for it and replace it with this.
 #
 
+import oods.sphinxtheme
 import os
 html_theme = 'sphinxtheme'
 html_theme_path = [os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'oods')]
