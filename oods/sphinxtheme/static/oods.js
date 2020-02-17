b88c12c2933dadba86df6d84a4253e5bd1e01a15
Jared Parnell <jared.parnell@opendataservices.coop>
2020-02-17 18:08:34 +0000

    adding sidebar logo and nav bar adjustments

--- oods/sphinxtheme/static/oods.js
+++ oods/sphinxtheme/static/oods.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
     fixLayout();
+    displayLogo();
     $( window ).resize(function() {
         fixLayout();
     });
@@ -8,4 +9,11 @@ $( document ).ready(function() {
 function fixLayout() {
     $('.oods-sidebar').css({'top':parseInt($('nav.navbar').height())+'px'});
     $('#oodsSideBar').width($('#oodsSideBarWrapper').width()+'px');
+}
+
+function displayLogo(){
+  var currPath = window.location.pathname;  
+  if (currPath == '/index.html'){
+    $('.oo-logo-sidebar').toggle();
+  }
 }
